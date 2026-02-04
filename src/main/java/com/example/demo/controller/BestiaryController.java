c6c13b632afa1be103ef53fb80889cc3ffa442d3
Romanariy <frog061205@gmail.com>
2026-02-04 22:26:56 +0500

    Update BestiaryController.java

--- src/main/java/com/example/demo/controller/BestiaryController.java
+++ src/main/java/com/example/demo/controller/BestiaryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/bestiary")
 public class BestiaryController {
